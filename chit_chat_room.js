0d1dcde8cb330a1550aae5ee2b8d9d5e37624d8b
Dhruv-2008 <79134023+Dhruv-2008@users.noreply.github.com>
2021-02-17 11:30:32 +0530

    Add files via upload

---
+++ chit_chat_room.js
@@ -0,0 +1,12 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyAxz6-CDKrI6V1PJkfR_lgkGnqkt9b0eEk",
+    authDomain: "chit-chat-9605c.firebaseapp.com",
+    databaseURL: "https://chit-chat-9605c-default-rtdb.firebaseio.com",
+    projectId: "chit-chat-9605c",
+    storageBucket: "chit-chat-9605c.appspot.com",
+    messagingSenderId: "171701748678",
+    appId: "1:171701748678:web:8ff5bdf0e7c4b48d7e28c7"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
--- lets_chat.js
+++ lets_chat.js
@@ -1,5 +1,5 @@
 function addUser(){
-    window.location="lets_chat_reoom.html";
+    window.location="lets_chat_room.html";
     var user_name=document.getElementById("user_name").value;
     localStorage.setItem("user_name", user_name);
 }
